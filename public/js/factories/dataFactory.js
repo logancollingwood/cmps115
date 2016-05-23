0dce3663c429a33747e737904fa653ac47ff2d52
logancollingwood <logan6694@gmail.com>
2016-05-22 17:34:43 -0700

    Fixing recent matches parsing

--- public/js/factories/dataFactory.js
+++ public/js/factories/dataFactory.js
@@ -69,10 +69,60 @@ angular.module('DataFactory', []).factory('DataFactory', ['$http', function($htt
             }
 
         }
+
+
         filtered.payload.runePages = pagesStruct;
         //end disgustingness
 
+        var recentMatches = filtered.payload.recentMatches;
+        for (var i = 0; i < recentMatches.length; i++) {
+            
+            switch (recentMatches[i]["lane"]) {
+                case "BOTTOM": 
+                    recentMatches[i]["lane"] = "Bot";
+                    break;
+                case "TOP": 
+                    recentMatches[i]["lane"] = "Top";
+                    break;
+                case "JUNGLE": 
+                    recentMatches[i]["lane"] = "Jungle";
+                    break;
+            }
 
+            switch (recentMatches[i]["role"]) {
+                case "DUO_SUPPORT": 
+                    recentMatches[i]["role"] = "Support";
+                    break;
+                case "DUO_CARRY": 
+                    recentMatches[i]["role"] = "ADC";
+                    break;
+                case "SOLO": 
+                    recentMatches[i]["role"] = "Solo";
+                    break;
+                case "NONE": 
+                    recentMatches[i]["role"] = "n/a";
+                    break;
+            }
+
+            switch (recentMatches[i]["won"]) {
+                case "0":
+                    recentMatches[i]["won"] = 0;
+                    break;
+                case "1":
+                    recentMatches[i]["won"] = 1;
+                    break;
+            }
+
+            switch (recentMatches[i]["first_blood"]) {
+                case "0":
+                    recentMatches[i]["first_blood"] = 0;
+                    break;
+                case "1":
+                    recentMatches[i]["first_blood"] = 1;
+                    break;
+            }
+
+        }
         return filtered;
     }
 
